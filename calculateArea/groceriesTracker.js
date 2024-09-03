e37118b20d7f46852be40c17fccdf76b7955e6a8
Zai Yang <yangz112504@gmail.com>
2024-09-03 00:08:16 -0400

    practice files done

---
+++ calculateArea/groceriesTracker.js
@@ -0,0 +1,7 @@
+function calculateTotal(){
+    let grocery1 = parseFloat(document.getElementById("grocery1").value);
+    let grocery2 = parseFloat(document.getElementById("grocery2").value);
+    let grocery3 = parseFloat(document.getElementById("grocery3").value);
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById("results").innerText = `The total amount is: \$${total}`;
+}
