0250bc3c7c0b2e2feb45f148aa472168e121b10d
Zai Yang <yangz112504@gmail.com>
2024-09-05 23:24:22 -0400

    finished practice

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -17,3 +17,21 @@ for(let i = 0;  i < dessertMenu.length; i++){
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
 }
 document.getElementById("dessertMenuItems").innerHTML = dessertItem;
+
+const breakfastMenuPrices = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const breakfastMenuPricesHTML = breakfastMenuPrices.map((item, index) =>`<p>Item ${index+1}: ${item}</p>`).join('');
+document.getElementById("breakfastMenuItems").innerHTML = breakfastMenuPricesHTML;
+
+let mainCourseItemPriceHTML = "";
+const mainCourseItemPrices = ['Steak -$24', 'Pasta -$15', 'Oatmeal -$8', 'Frittata -$17'];
+mainCourseItemPrices.forEach(function(item, index){
+    mainCourseItemPriceHTML+=`<p>Item ${index+1}: ${item}`; 
+});
+document.getElementById("maincourseMenuItems").innerHTML = mainCourseItemPriceHTML;
+
+let dessertItemPricesHTML = "";
+const dessertMenuItemPrices = ["Cake -$10", "Ice Cream -$5", "Pudding -$3", "Fruit Salad -$69.69"];
+for(let i = 0; i < dessertMenuItemPrices.length; i++){
+    dessertItemPricesHTML+=`<p>Item ${i+1}: ${dessertMenuItemPrices[i]}</p>`;
+}
+document.getElementById("dessertMenuItems").innerHTML = dessertItemPricesHTML;
